faff479f30023bb26881604c0c6ba3c7f5e908d2
Luka Agic <agic.luke@gmail.com>
2023-03-15 08:54:54 +0100

    Initial commit, practice methods

---
+++ MethodsClassesObjectsPractice/MCO/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            string message = Console.ReadLine();
+
+            Reverse(message);
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        private static void Reverse(string message)
+        {
+
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+
+            foreach (char item in messageArray)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
