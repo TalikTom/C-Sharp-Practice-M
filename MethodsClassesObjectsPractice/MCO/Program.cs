52dcd921175226042ea3d4e2d914ac9c20c00f50
Luka Agic <agic.luke@gmail.com>
2023-03-15 11:28:15 +0100

    Practice classes, greeting message

--- MethodsClassesObjectsPractice/MCO/Program.cs
+++ MethodsClassesObjectsPractice/MCO/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MCO;
 using System;
 
 namespace MyApp // Note: actual namespace depends on the project name.
@@ -7,27 +8,34 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            HelloWorld();
-            string message = Console.ReadLine();
+            Console.Write("What is your first name?\n");
+            string firstName = Console.ReadLine();
 
-            Reverse(message);
-        }
+            UserMessages.ApplicationStartMessage(firstName);
 
-        private static void HelloWorld()
-        {
-            Console.WriteLine("Hello World");
-        }
 
-        private static void Reverse(string message)
-        {
 
-            char[] messageArray = message.ToCharArray();
-            Array.Reverse(messageArray);
+            //    HelloWorld();
+            //    string message = Console.ReadLine();
 
-            foreach (char item in messageArray)
-            {
-                Console.Write(item);
-            }
+            //    Reverse(message);
+            //}
+
+            //private static void HelloWorld()
+            //{
+            //    Console.WriteLine("Hello World");
+            //}
+
+            //private static void Reverse(string message)
+            //{
+
+            //    char[] messageArray = message.ToCharArray();
+            //    Array.Reverse(messageArray);
+
+            //    foreach (char item in messageArray)
+            //    {
+            //        Console.Write(item);
+            //    }
         }
     }
 }
---
+++ MethodsClassesObjectsPractice/MCO/UserMessages.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCO
+{
+    public class UserMessages
+    {
+        public static void ApplicationStartMessage(string firstName)
+        {
+            Console.WriteLine($"Welcome {firstName}");
+
+
+            //check current hour and display a message accordingly
+
+            int hourOfDay = DateTime.Now.Hour;
+
+            if (hourOfDay < 12)
+            {
+                Console.WriteLine("Good morning!");
+            }
+            else if (hourOfDay < 19)
+            {
+                Console.WriteLine("Good afternoon!");
+            }
+            else
+            {
+                Console.WriteLine("Good evening!");
+            }
+        }
+
+    }
+}
