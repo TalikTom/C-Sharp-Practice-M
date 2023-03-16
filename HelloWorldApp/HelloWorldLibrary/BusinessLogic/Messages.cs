a5729a45f99467a6116a7dd712bf1d336b82fb6a
Luka Agic <agic.luke@gmail.com>
2023-03-16 12:16:35 +0100

    Add try catch

--- HelloWorldApp/HelloWorldLibrary/BusinessLogic/Messages.cs
+++ HelloWorldApp/HelloWorldLibrary/BusinessLogic/Messages.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using HelloWorldLibrary.Models;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace HelloWorldLibrary.BusinessLogic;
 
@@ -14,6 +16,32 @@ public class Messages
 
     private string LookUpCustomText(string key, string language)
     {
+        JsonSerializerOptions options = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
 
+        try
+        {
+            List<CustomText>? messageSets = JsonSerializer
+           .Deserialize<List<CustomText>>
+           (
+           File.ReadAllText("CustomText.json"), options
+           );
+
+            CustomText? messages = messageSets?.Where(x => x.Language == language).First();
+
+            if (messages is null)
+            {
+                throw new NullReferenceException("The specified language was not found in json file");
+            }
+
+            return messages.Translations[key];
+        }
+        catch (Exception ex)
+        {
+            _log.LogError("Error looking up the custom text", ex);
+            throw;
+        }
     }
 }
