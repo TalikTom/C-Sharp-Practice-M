8b166085c9ac9c82029f263964ade7abc2e244ec
Luka Agic <agic.luke@gmail.com>
2023-03-16 12:05:17 +0100

    Initial commit, add class library, seperate business logic

---
+++ HelloWorldApp/HelloWorldLibrary/BusinessLogic/Messages.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace HelloWorldLibrary.BusinessLogic;
+
+public class Messages
+{
+    private readonly ILogger<Messages> _log;
+
+    public Messages(ILogger<Messages> log)
+    {
+        _log = log;
+    }
+
+
+    private string LookUpCustomText(string key, string language)
+    {
+
+    }
+}
