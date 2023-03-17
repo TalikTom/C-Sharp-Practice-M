67f70de5285f77e26009140daaa81c048a81a381
Luka Agic <agic.luke@gmail.com>
2023-03-17 10:42:52 +0100

    Initial commit CRU txt file, add assignments

---
+++ CRUFilesApp/CRUFiles/Program.cs
@@ -0,0 +1,16 @@
+﻿//create txt file
+//read txt file
+//update txt file
+//copy one file to another
+
+
+namespace CRUFilesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
