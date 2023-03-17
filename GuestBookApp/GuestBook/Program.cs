1761f00adcb6e903210b1dfa7e6598148c21e559
Luka Agic <agic.luke@gmail.com>
2023-03-17 10:01:26 +0100

    Initial commit GuestBook App

---
+++ GuestBookApp/GuestBook/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GuestBookLibrary.Model;
+
+
+// Capture the information about each guest (assumption is at least one guest and unknow maximum)
+// Info to capture: first name, last name, message to the host
+// Once done, loop through each guest and print their info
+
+
+
+List<GuestModel> guests = new List<GuestModel>();
+
+string moreGuestsComing = "";
+
+do
+{
+    GuestModel guest = new GuestModel();
+    Console.Write("What is your first name: ");
+} while (moreGuestsComing.ToLower() ! = "yes");
+
+Console.ReadLine();
