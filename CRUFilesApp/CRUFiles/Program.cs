748d1ade6802917c17321a6767ca7bf3691207dd
Luka Agic <agic.luke@gmail.com>
2023-03-17 10:51:28 +0100

    Create directory

--- CRUFilesApp/CRUFiles/Program.cs
+++ CRUFilesApp/CRUFiles/Program.cs
@@ -10,7 +10,28 @@ namespace CRUFilesApp
     {
         static void Main(string[] args)
         {
-            
+            // Create a string array with the lines of text
+            string[] lines = { "First line", "Second line", "Third line" };
+
+            // Set a variable to the Documents path.
+            string docPath = $"C:\\Users\\student\\Documents\\Luka";
+
+
+            if (!Directory.Exists(docPath))
+                {
+                Directory.CreateDirectory(docPath);
+            }
+
+
+
+            //Console.WriteLine(docPath);
+
+            //// Write the string array to a new file named "WriteLines.txt".
+            //using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt")))
+            //{
+            //    foreach (string line in lines)
+            //        outputFile.WriteLine(line);
+            //}
         }
     }
 }
