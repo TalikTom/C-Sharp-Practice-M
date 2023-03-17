22973ddc093ede05308c7f7950d6cd1923103c28
Luka Agic <agic.luke@gmail.com>
2023-03-17 11:10:18 +0100

    FileStream write text

--- CRUFilesApp/CRUFiles/Program.cs
+++ CRUFilesApp/CRUFiles/Program.cs
@@ -4,34 +4,51 @@
 //copy one file to another
 
 
+using System.Text;
+
 namespace CRUFilesApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Create a string array with the lines of text
-            string[] lines = { "First line", "Second line", "Third line" };
+            //// Create a string array with the lines of text
+            //string[] lines = { "First line", "Second line", "Third line" };
 
-            // Set a variable to the Documents path.
-            string docPath = $"C:\\Users\\student\\Documents\\Luka";
-
-
-            if (!Directory.Exists(docPath))
-                {
-                Directory.CreateDirectory(docPath);
-            }
+            //// Set a variable to the Documents path.
+            //string docPath = $"C:\\Users\\student\\Documents\\Luka";
 
 
-
-            //Console.WriteLine(docPath);
-
-            //// Write the string array to a new file named "WriteLines.txt".
-            //using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt")))
-            //{
-            //    foreach (string line in lines)
-            //        outputFile.WriteLine(line);
+            //if (!Directory.Exists(docPath))
+            //    {
+            //    Directory.CreateDirectory(docPath);
             //}
+
+
+            //string fileLocation = Path.Combine(docPath, "WriteLines.txt");
+
+
+            //using (FileStream stream = new FileStream(fileLocation,FileMode.Open))
+            //{
+            //    StreamWriter writer = new StreamWriter(stream);
+            //    foreach (string line in lines)
+            //    writer.WriteLine(line);
+            //    writer.Close();
+            //    writer.Flush();
+            //}   
+
+
+            //C# FileStream write text
+
+            string fileName = $"C:\\Users\\student\\Documents\\Luka\\test.txt";
+
+            using FileStream fs = File.OpenWrite(fileName);
+
+            var data = "testing";
+            byte[] bytes = Encoding.UTF8.GetBytes(data);
+
+            fs.Write(bytes, 0, bytes.Length);
+
         }
     }
 }
